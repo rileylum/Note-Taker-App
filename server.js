30ea6e5116182bee0913f34e96d5428745ffb999
Riley Lum <rileylum@adam.com.au>
2021-08-17 17:35:00 +0930

    add api get path

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
+        res.json(JSON.parse(data));
+    })
 })
 
 app.get('*', (req, res) => {
